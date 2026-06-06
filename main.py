2b78939780da2d9d38bacf175b7ba51610d9dcce
clauisa040104-png <clauisa040104@gmail.com>
2026-06-05 21:33:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-import tensorflow as tf
+import tflite_runtime.interpreter as tflite
 import numpy as np
 from PIL import Image
 import io
@@ -14,10 +14,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Cargar modelo
-model = tf.keras.models.load_model('azalea_model.h5')
+# Cargar modelo .tflite
+interpreter = tflite.Interpreter(model_path='azalea_model.tflite')
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-# Clases
 labels = ['botrytis', 'mancha_foliar', 'no_azalea', 'oidio', 'phytophthora', 'roya_en_hoja', 'sana']
 
 @app.get("/")
@@ -29,13 +31,16 @@ async def predecir(file: UploadFile = File(...)):
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert('RGB')
     image = image.resize((224, 224))
-    img_array = np.array(image) / 255.0
+    img_array = np.array(image, dtype=np.float32) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
+
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details[0]['index'])
     
-    prediccion = model.predict(img_array)
-    clase_index = np.argmax(prediccion)
-    confianza = float(np.max(prediccion))
-    
+    clase_index = np.argmax(output)
+    confianza = float(np.max(output))
+
     return {
         "enfermedad": labels[clase_index],
         "confianza": round(confianza * 100, 2)
