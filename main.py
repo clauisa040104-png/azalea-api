05567e021e8510884442e0a8bb2d33dcc452e5bb
clauisa040104-png <clauisa040104@gmail.com>
2026-07-18 12:39:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ async def chatbot(req: ChatRequest):
         mensajes.append({"role": "user", "content": req.mensaje})
 
         response = client.chat.completions.create(
-            model="mistralai/mistral-7b-instruct:free",
+            model="google/gemma-3-4b-it:free",
             messages=mensajes,
             max_tokens=500,
             temperature=0.7
